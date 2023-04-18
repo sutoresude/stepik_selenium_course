487590633a32f38207af9285ecb5982008edcc15
a.kovalchuk <napstermfox@gmail.com>
2023-04-18 20:51:53 +0500

    квест с аллертом

---
+++ stepik_allert_confirm.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_el = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_el.text
+    y = calc(x)
+    browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(y)
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
