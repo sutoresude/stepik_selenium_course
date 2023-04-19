5b13d5b1742fa0e3d6679a0db8e72ae4eec813d6
a.kovalchuk <napstermfox@gmail.com>
2023-04-19 17:13:29 +0500

    rework, add new fixture

--- test_fixture.py
+++ test_fixture.py
@@ -6,10 +6,16 @@ import pytest
 link = "http://suninjuly.github.io/registration1.html"
 link2 = "http://suninjuly.github.io/registration2.html"
 
-@pytest.fixture()
+@pytest.fixture
 def setup():
-    browser = webdriver.Chrome()
-    return browser
+    setup = webdriver.Chrome()
+    yield setup
+    setup.quit()
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
 
 class Tests():
 
@@ -23,7 +29,7 @@ class Tests():
         time.sleep(1)
         welcome_text_elt = setup.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elt.text
-        assert "Congratulations! You have successfully registered" == welcome_text
+        assert "Congratulations! You have successfully registered!" == welcome_text
     def test_2(self, setup):
         setup.get(link2)
         setup.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.first").send_keys(123)
